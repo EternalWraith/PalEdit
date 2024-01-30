31ef6cf2f5fe5292f079785da41e15332d05beac
Jacob Lawrence <jajay2000@live.com>
2024-01-30 10:19:31 +0000

    Switched from PyToExe to CxFreeze

---
+++ CxFreezeCompile.py
@@ -0,0 +1,18 @@
+import sys, os
+from cx_Freeze import setup, Executable
+
+build_options = {
+    "excludes": [],
+    "zip_include_packages": [],
+    "include_files": ["resources/"],
+}
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name = "PalEdit v0.5-pre",
+    version = "0.5-pre",
+    description = "A simple tool for editing PalWorld saves",
+    options={"build_exe": build_options},
+    executables=[Executable("PalEdit.py", base=base, icon="resources/MossandaIcon.ico")],
+)
